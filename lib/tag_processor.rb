eb1f5acfd6e5d11473c9cc0aa04903020b1fef8e
Tom Dooner <tom.dooner@brigade.com>
2016-10-13 23:40:51 -0700

    Add source_type to query too

--- lib/tag_processor.rb
+++ lib/tag_processor.rb
@@ -2,12 +2,10 @@ module TagProcessor
   class Processor
     def initialize
       TagProcessor::Base.class_variable_get(:@@processors).each do |processor_klass|
-        puts "Checking processor: #{processor_klass}"
         processor_revision = processor_klass.class_variable_get(:@@revision)
 
         processor_klass.each_model do |model|
           TagProcessingRecord
-            .where(source_type: model.name)
             .where(processor: processor_klass.name)
             .where.not(revision: processor_revision)
             .destroy_all
@@ -15,6 +13,7 @@ module TagProcessor
           model.joins(
             "LEFT OUTER JOIN tag_processing_records
                ON tag_processing_records.source_id = #{model.table_name}.id
+              AND tag_processing_records.source_type = '#{model.name}'
               AND processor = '#{processor_klass.name}'")
             .where(tag_processing_records: { id: nil })
             .find_each do |record|
